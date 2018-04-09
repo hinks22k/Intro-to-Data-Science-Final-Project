6e582c31a0d9d30cd4a646a5d78bf00a70f17a83
amaneel101 <aneelapriyantha@gmail.com>
2018-04-08 20:25:32 -0400

    Add files via upload
    wrote methods for interpreting data; just need the data to put into it

--- final_proj.py
+++ final_proj.py
@@ -1,4 +1,6 @@
-
+import requests
+from math import sqrt
+from functools import reduce
 
 def readFile( fileName ):
     """
@@ -11,5 +13,51 @@ def readFile( fileName ):
         line = file.readline()
     file.close()
 
+def mean( lst ):
+    """
+    mean takes a single argument, a list of numbers, \
+    and returns the mean value for the numbers in that list.
+    """
+    return( sum( lst ) / len( lst ) )
 
-print( readFile( 'income.py' ) )
+# may be wrong
+def variance( lst ):
+    """
+    variance takes a list of numbers and returns the \
+    population variance within that list.
+    """
+    meanOfList = mean( lst )
+    # subtracts num in list from average and squares it
+    # places that number into a new list
+    sumOfDifferences = map( lambda x: ( x - meanOfList ) ** 2, lst )
+    return mean( list( sumOfDifferences ) )
+
+def standard_deviation( lst ):
+    return sqrt( variance( lst ) )
+
+def myMin( lst ):
+    """
+    myMin takes a list of numbers as its argument and \
+    returns the minimum value in the list.
+    """
+    # Using reduce and the < sign to return the min value
+    return reduce( lambda x,y: x if x < y else y, lst)
+
+def myMax( lst ):
+    """
+    myMax takes a list of numbers as its argument and \
+    returns the maximum value in the list.
+    """
+    # Using reduce and the > sign to return the max value
+    return reduce( lambda x,y: x if x > y else y, lst)
+
+def filterOutMinMax( lst ):
+    """
+    filterOutMinMax takes a single list of numbers as its only argument. \
+    filterOutMinMax should return a list that has the same elements as \
+    the original list except for the original’s min and max values.
+    """
+    minOfList = myMin( lst )
+    maxOfList = myMax( lst )
+    # filter out the min and max values of the list
+    return list( filter( lambda x: x != minOfList and x != maxOfList, lst ) )
